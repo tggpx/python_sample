8131d2d38908749b7b50a5f701eb094b1e4d2b42
jielin <lin_work0@163.com>
2018-12-17 16:17:20 +0800

    1、修改格式错误，2、增加case父类，3、修改编码错误问题

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,56 +1,79 @@
-#!/usr/bin/env python
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 import subprocess
-class case_cgi_file(object):
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class base_case(object):
+    '''Parent for case handlers.'''
+
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, "index.html")
+
+    def test(self, handler):
+        assert False, 'Not implemented'
+
+    def act(self, handler):
+        assert False, 'Not implemented'
+
+
+class case_cgi_file(base_case):
     def test(self, handler):
         return os.path.isfile(handler.full_path) and \
                 handler.full_path.endswith('.py')
-    
+
     def act(self, handler):
         handler.run_cgi(handler.full_path)
 
-class case_no_file(object):
+
+class case_no_file(base_case):
     '''case file is not exists'''
+
     def test(self, handler):
         return not os.path.exists(handler.full_path)
-    
+
     def act(self, handler):
         raise ServerException("'{0}' not found".format(handler.path))
 
-class case_existing_file(object):
+
+class case_existing_file(base_case):
     '''case file is exists'''
+
     def test(self, handler):
         return os.path.isfile(handler.full_path)
-    
-    def act(self, handler):
-        handler.handle_file(handler.full_path)
 
-class case_always_fail(object):
+    def act(self, handler):
+        self.handle_file(handler, handler.full_path)
+
+
+class case_always_fail(base_case):
     def test(self, handler):
         return True
-    
+
     def act(self, handler):
         raise ServerException("Unknown object '{0}'".format(handler.path))
 
-class case_directory_index_file(object):
-    '''Serve index.html page for a directory.'''
 
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
+class case_directory_index_file(base_case):
+    '''Serve index.html page for a directory.'''
 
     def test(self, hander):
         return os.path.isdir(hander.full_path) and \
                 os.path.isfile(self.index_path(hander))
 
     def act(self, handler):
-        handler.handle_file(self.index_path(handler)) 
+        self.handle_file(handler, self.index_path(handler))
 
-class case_directory_no_index_file(object):
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
-    
+
+class case_directory_no_index_file(base_case):
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                not os.path.isfile(self.index_path(handler))
@@ -58,9 +81,11 @@ class case_directory_no_index_file(object):
     def act(self, handler):
         handler.list_dir(handler.full_path)
 
+
 class ServerException(Exception):
     '''Server inner error'''
     pass
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     '''Handle HTTP request by returning a fixed 'page'.'''
@@ -73,7 +98,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         </body>
     </html>
     '''
-    
+
     List_Page = ''' \
     <html>
         <body>
@@ -86,45 +111,50 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def run_cgi(self, full_path):
         cmd = 'python ' + full_path
-        out = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out = subprocess.Popen(
+            cmd,
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
         out.stdin.close()
-        data = str(out.stdout.read(), 'utf-8')
+        data = out.stdout.read()
         out.stdout.close()
         self.send_content(data)
 
     def list_dir(self, full_path):
         try:
             entries = os.listdir(full_path)
-            bullets = ['<li>{0}</li>'.format(e) for e in entries if not e.startswith('.')]
-            page = self.List_Page.format('\n'.join(bullets))
+            bullets = [
+                '<li>{0}</li>'.format(e) for e in entries
+                if not e.startswith('.')
+            ]
+            page = self.List_Page.format('\n'.join(bullets)).encode()
             self.send_content(page)
         except OSError as msg:
             self.handle_error(msg)
 
     def handle_error(self, msg):
-        content = self.Error_Page.format(path = self.path, msg=msg)
+        content = self.Error_Page.format(path=self.path, msg=msg).encode()
         self.send_content(content, 404)
-
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'r') as reader:
-                contant = reader.read()
-            self.send_content(contant)
-        except IOError as msg:
-            msg = "'{0}' cannot be read:{1}".format(self.path, msg)
-            self.handle_error(msg)
 
     def send_content(self, content, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
-        self.send_header('Content-Length',str(len(content)))
+        self.send_header('Content-Length', str(len(content)))
         self.end_headers()
-        self.wfile.write(content.encode())
+        self.wfile.write(content)
 
-    Cases = [case_no_file, case_cgi_file, case_existing_file, case_directory_index_file, case_directory_no_index_file, case_always_fail]
-    #Handle a Get request.
+    Cases = [
+        case_no_file, case_cgi_file, case_existing_file,
+        case_directory_index_file, case_directory_no_index_file,
+        case_always_fail
+    ]
+
+    # Handle a Get request.
+
     def do_GET(self):
-        #self.send_page(self.create_page())
+        # self.send_page(self.create_page())
         try:
             # Figure out what exactly is being requested.
             self.full_path = os.getcwd() + self.path
